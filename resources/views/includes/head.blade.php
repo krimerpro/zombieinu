9d01a483de7f8041c2686c380bcef9793aea6e59
hadron megantara <hadronmegantara@gmail.com>
2021-04-12 17:42:55 +0700

    Initial commit

---
+++ resources/views/includes/head.blade.php
@@ -0,0 +1,26 @@
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=0, minimal-ui">
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
+
+<meta name="description" content="WINCAT Token | BSC">
+<meta name="keywords" content="crypto, wincat, wincattoken, bitcoin, bsc, binance, binancesmartchain">
+<meta name="author" content="WINCAT">
+
+<title>WINCAT Token | BSC</title>
+
+<!-- Fonts -->
+<link href="https://fonts.googleapis.com/css?family=Comfortaa:300,400,500,700" rel="stylesheet">
+
+<link href="{{ asset('css/font-awesome.min.css') }}" rel="stylesheet">
+
+<link href="{{ asset('css/app.css') }}" rel="stylesheet" type="text/css">
+<link href="{{ asset('css/style.min.css') }}" rel="stylesheet" type="text/css">
+<link href="{{ asset('css/flag-icon.min.css') }}" rel="stylesheet" type="text/css">
+<link href="{{ asset('css/animate.min.css') }}" rel="stylesheet" type="text/css">
+<link href="{{ asset('css/flipclock.css') }}" rel="stylesheet" type="text/css">
+<link href="{{ asset('css/swiper.min.css') }}" rel="stylesheet" type="text/css">
+
+<link href="{{ asset('css/demo.min.css') }}" rel="stylesheet" type="text/css">
+
+<link href="{{ asset('css/template-3d-animation.css') }}" rel="stylesheet" type="text/css">
---
+++ resources/views/includes/header.blade.php
@@ -0,0 +1,81 @@
+<!-- Preloader | Comment below code if you don't want preloader-->
+<div id="loader-wrapper">
+<svg viewbox=" 0 0 512 512" id="loader">
+    <linearGradient id="loaderLinearColors" x1="0" y1="0" x2="1" y2="1">
+        <stop offset="5%" stop-color="#28bcfd"></stop>
+        <stop offset="100%" stop-color="#1d78ff"></stop>
+    </linearGradient>
+    <g>
+        <circle cx="256" cy="256" r="150" fill="none" stroke="url(#loaderLinearColors)" />
+    </g>
+    <g>
+        <circle cx="256" cy="256" r="125" fill="none" stroke="url(#loaderLinearColors)" />
+    </g>
+    <g>
+        <circle cx="256" cy="256" r="100" fill="none" stroke="url(#loaderLinearColors)" />
+    </g>
+    <g>
+        <circle cx="256" cy="256" r="75" fill="none" stroke="url(#loaderLinearColors)" />
+    </g>
+    <circle cx="256" cy="256" r="60" fill="url(#loaderImage)" stroke="none" stroke-width="0" />
+
+    <!-- Change the preloader logo here -->
+    <defs>
+        <pattern id="loaderImage" height="100%" width="100%" patternContentUnits="objectBoundingBox">
+            <image href="{{ asset('images/logo.jpeg') }}" preserveAspectRatio="none" width="1" height="1"></image>
+        </pattern>
+    </defs>
+</svg>
+
+<div class="loader-section section-left"></div>
+<div class="loader-section section-right"></div>
+</div>
+<!--/ Preloader --><nav class="vertical-social">
+<ul>
+    <li><a href="#"><i class="fa fa-telegram" aria-hidden="true"></i></a></li>
+    <li><a href="#"><i class="fa fa-medium" aria-hidden="true"></i></a></li>
+    <li><a href="#"> <i class="fa fa-twitter" aria-hidden="true"></i></a></li>
+    <li><a href="#"><i class="fa fa-github" aria-hidden="true"></i></a></li>
+</ul>
+</nav>
+
+<!-- /////////////////////////////////// HEADER /////////////////////////////////////-->
+
+<!-- Header Start-->
+<header class="page-header">
+  <!-- Horizontal Menu Start-->
+  <nav class="main-menu static-top navbar-dark navbar navbar-expand-lg fixed-top mb-1"><div class="container">
+
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarCollapse" aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+  </button>
+<div class="collapse navbar-collapse" id="navbarCollapse">
+    <div id="navigation" class="navbar-nav" style="width:100%">
+        <ul class="navbar-nav mt-1">
+            <li class="nav-item animated" data-animation="fadeInDown" data-animation-delay="1.1s">
+                <a class="nav-link" href="/" style="color:pink">WINCAT</a>
+            </li>
+            <li class="nav-item animated" data-animation="fadeInDown" data-animation-delay="1.1s">
+                <a class="nav-link" href="#tokenomics">Tokenomics</a>
+            </li>
+            <li class="nav-item animated" data-animation="fadeInDown" data-animation-delay="1.1s">
+                <a class="nav-link" href="#about">About</a>
+            </li>
+            <li class="nav-item animated" data-animation="fadeInDown" data-animation-delay="1.3s">
+                <a class="nav-link" href="index.html#whitepaper">Whitepaper</a>
+            </li>
+            <li class="nav-item animated" data-animation="fadeInDown" data-animation-delay="1.5s">
+                <a class="nav-link" href="index.html#roadmap">Roadmap</a>
+            </li>
+        </ul>
+        <span id="slide-line"></span>
+        <form class="form-inline mt-2 mt-md-0 text-right" style="margin-left:auto">
+            <a class="btn btn-sm btn-gradient-purple btn-glow my-2 my-sm-0 animated" data-animation="fadeInDown" data-animation-delay="1.8s" href="#" target="_blank">NFT Market</a>
+        </form>
+    </div>
+</div>
+</div>
+  </nav>
+  <!-- /Horizontal Menu End-->
+</header>
+<!-- /Header End-->
