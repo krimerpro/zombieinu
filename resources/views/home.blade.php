bf5a69634ac5f637e3a6077cb64b5c92f5336d0a
varanustoken <varanustoken@gmail.com>
2022-09-19 13:04:30 +0700

    update

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -16,8 +16,8 @@
                         <h1 class="best-template animated" data-animation="fadeInUpShorter" data-animation-delay="1.5s">ARABIAN APE GROW</h1>
                         <h3 class="d-block white animated" data-animation="fadeInUpShorter" data-animation-delay="1.6s">The Next Generation of NFT Marketplace</h3>
                         <div class="mt-5">
-                            <a href="https://exchange.pancakeswap.finance/#/swap?inputCurrency=0x0356a0E3e6068099b3c1f85D41E2E854d583b83C" target="_blank" class="btn btn-lg btn-gradient-purple btn-glow mr-4 mb-2 animated" data-animation="fadeInUpShorter" data-animation-delay="1.7s">Buy on PancakeSwap</a>
-                            <a href="https://bscscan.com/token/0x0356a0E3e6068099b3c1f85D41E2E854d583b83C" class="btn btn-lg btn-gradient-purple btn-glow mb-2 animated" target="_blank" data-animation="fadeInUpShorter" data-animation-delay="1.8s">BscScan Info</a>
+                            <a href="https://exchange.pancakeswap.finance/#/swap?inputCurrency=" target="_blank" class="btn btn-lg btn-gradient-purple btn-glow mr-4 mb-2 animated" data-animation="fadeInUpShorter" data-animation-delay="1.7s">Buy on PancakeSwap</a>
+                            <a href="https://bscscan.com/token/" class="btn btn-lg btn-gradient-purple btn-glow mb-2 animated" target="_blank" data-animation="fadeInUpShorter" data-animation-delay="1.8s">BscScan Info</a>
                         </div>
                     </div>
                 </div>
