9a52bf8878661145c49b1044d7ea66e939031ddb
varanustoken <varanustoken@gmail.com>
2022-09-23 19:19:12 +0700

    progress

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -479,7 +479,7 @@
                     <span class="fa fa-table-columns" style="color:blue"></span> Token Supply : 1.000.000.000.000.000
                   </li>
                   <li style="margin-top:10px">
-                    <span class="fa fa-table-columns" style="color:blue"></span> Initial Liquidity Pool : 4 BNB
+                    <span class="fa fa-table-columns" style="color:blue"></span> Initial Liquidity Pool : 3 BNB
                   </li>
               </ul>
             </div>
