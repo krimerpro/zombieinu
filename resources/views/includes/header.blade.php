d24481318abe8a6aff46c48979362d7a9cb51d7a
varanustoken <varanustoken@gmail.com>
2023-02-20 13:24:35 +0700

    a

--- resources/views/includes/header.blade.php
+++ resources/views/includes/header.blade.php
@@ -85,7 +85,7 @@
                         <h1>Best Machine Learning Product Creator</h1>
                         <p>Transforming businesses with Artificial Intelligence</p>
                         <div class="button-group button-group-animated">
-                            <a href="https://poocoin.app/tokens/0x5CcD544099506dBCDA162224c44c1a44964467e3" class="btn main-btn">Poocoin Chart</a>
+                            <a href="https://poocoin.app/tokens/0x0fDeca4BfE167DEB7E86a064B6734CA13120F276" class="btn main-btn">Poocoin Chart</a>
                             <a href="https://t.me/ErudexAiPortal" class="btn main-btn main-btn-white">Telegram Group</a>
                         </div>
                     </div>
